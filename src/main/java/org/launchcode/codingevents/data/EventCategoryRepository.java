c31b66f44a1c0aac4cf5dfe7e8a78ff74215af68
JayhawkChief <bxrayhamm@gmail.com>
2020-03-05 16:27:41 -0600

    In the process of coding events exercises

---
+++ src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
@@ -0,0 +1,12 @@
+package org.launchcode.codingevents.data;
+
+import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventCategory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventCategoryRepository {
+
+//    static Object findAll() {
+//    }
+}
